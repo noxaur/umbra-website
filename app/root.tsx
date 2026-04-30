50bdc82fdbe1012de83c1997fa513b99f2fa4a22
nox <nox.heights@gmail.com>
2026-05-01 00:53:54 +0200

    Added favicon and tilte

--- app/root.tsx
+++ app/root.tsx
@@ -41,6 +41,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="../public/umbra.svg"></link>
+        <title>Umbra</title>
         <Meta />
         <Links />
         {/* Blocking script  */}
