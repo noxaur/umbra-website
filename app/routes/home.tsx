d40a530f62b86ef32d668478b5dcdc35b2a6bb7a
nox <nox.heights@gmail.com>
2026-05-02 01:17:02 +0200

    Added image embed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,7 +5,7 @@ export function meta({}: Route.MetaArgs) {
     return [
         {title: "Umbra"},
         {name: "description", content: "Join Umbra today!"},
-        {property: "og:image", content: "https://shl52fj0cqdjroa6.public.blob.vercel-storage.com/umbra.jpg"},
+        {property: "og:image", content: "/umbra.svg"},
     ];
 }
 
