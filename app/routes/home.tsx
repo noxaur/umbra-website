6940ea4d1f82815d022d2d6aafd3d12e6d345a3e
nox <nox.heights@gmail.com>
2026-05-02 01:17:02 +0200

    Added image embed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,14 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
 import LandingPage from "../Home/main";
 
 export function meta({}: Route.MetaArgs) {
-	return [
-		{ title: "Umbra" },
-		{ name: "description", content: "Join Umbra today!" },
-	];
+    return [
+        {title: "Umbra"},
+        {name: "description", content: "Join Umbra today!"},
+        {property: "og:image", content: "/umbra.svg"},
+    ];
 }
 
 export default function Home() {
-	return <LandingPage />;
+    return <LandingPage/>;
 }
